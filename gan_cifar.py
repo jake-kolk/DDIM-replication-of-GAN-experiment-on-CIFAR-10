b9eddfc21d088a082f6250616c0142cb6581eabc
jake-kolk <157997841+jake-kolk@users.noreply.github.com>
2025-10-26 14:13:20 -0700

    Update gan_cifar.py

--- gan_cifar.py
+++ gan_cifar.py
@@ -145,53 +145,54 @@ niter = 25
 g_loss = []
 d_loss = []
 
-for epoch in range(niter):
-    for i, data in enumerate(dataloader, 0):
-        ############################
-        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-        ###########################
-        # train with real
-        netD.zero_grad()
-        real_cpu = data[0].to(device)
-        batch_size = real_cpu.size(0)
-        label = torch.full((batch_size,), real_label, device=device)
-
-        output = netD(real_cpu)
-        errD_real = criterion(output, label)
-        errD_real.backward()
-        D_x = output.mean().item()
-
-        # train with fake
-        noise = torch.randn(batch_size, nz, 1, 1, device=device)
-        fake = netG(noise)
-        label.fill_(fake_label)
-        output = netD(fake.detach())
-        errD_fake = criterion(output, label)
-        errD_fake.backward()
-        D_G_z1 = output.mean().item()
-        errD = errD_real + errD_fake
-        optimizerD.step()
-
-        ############################
-        # (2) Update G network: maximize log(D(G(z)))
-        ###########################
-        netG.zero_grad()
-        label.fill_(real_label)  # fake labels are real for generator cost
-        output = netD(fake)
-        errG = criterion(output, label)
-        errG.backward()
-        D_G_z2 = output.mean().item()
-        optimizerG.step()
-
-        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f' % (epoch, niter, i, len(dataloader), errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
-        
-        #save the output
-        if i % 100 == 0:
-            print('saving the output')
-            vutils.save_image(real_cpu,'output/real_samples.png',normalize=True)
-            fake = netG(fixed_noise)
-            vutils.save_image(fake.detach(),'output/fake_samples_epoch_%03d.png' % (epoch),normalize=True)
-    
-    # Check pointing for every epoch
-    torch.save(netG.state_dict(), 'weights/netG_epoch_%d.pth' % (epoch))
-    torch.save(netD.state_dict(), 'weights/netD_epoch_%d.pth' % (epoch))
+if __name__ == "__main__":
+  for epoch in range(niter):
+      for i, data in enumerate(dataloader, 0):
+          ############################
+          # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+          ###########################
+          # train with real
+          netD.zero_grad()
+          real_cpu = data[0].to(device)
+          batch_size = real_cpu.size(0)
+          label = torch.full((batch_size,), real_label, device=device)
+  
+          output = netD(real_cpu)
+          errD_real = criterion(output, label)
+          errD_real.backward()
+          D_x = output.mean().item()
+  
+          # train with fake
+          noise = torch.randn(batch_size, nz, 1, 1, device=device)
+          fake = netG(noise)
+          label.fill_(fake_label)
+          output = netD(fake.detach())
+          errD_fake = criterion(output, label)
+          errD_fake.backward()
+          D_G_z1 = output.mean().item()
+          errD = errD_real + errD_fake
+          optimizerD.step()
+  
+          ############################
+          # (2) Update G network: maximize log(D(G(z)))
+          ###########################
+          netG.zero_grad()
+          label.fill_(real_label)  # fake labels are real for generator cost
+          output = netD(fake)
+          errG = criterion(output, label)
+          errG.backward()
+          D_G_z2 = output.mean().item()
+          optimizerG.step()
+  
+          print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f' % (epoch, niter, i, len(dataloader), errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+          
+          #save the output
+          if i % 100 == 0:
+              print('saving the output')
+              vutils.save_image(real_cpu,'output/real_samples.png',normalize=True)
+              fake = netG(fixed_noise)
+              vutils.save_image(fake.detach(),'output/fake_samples_epoch_%03d.png' % (epoch),normalize=True)
+      
+      # Check pointing for every epoch
+      torch.save(netG.state_dict(), 'weights/netG_epoch_%d.pth' % (epoch))
+      torch.save(netD.state_dict(), 'weights/netD_epoch_%d.pth' % (epoch))
